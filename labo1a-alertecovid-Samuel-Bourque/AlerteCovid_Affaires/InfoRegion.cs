4f4f3b2b608ff6d97b5360f5431accdf627f196e
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 00:34:07 -0500

    Add files via upload

---
+++ labo1a-alertecovid-Samuel-Bourque/AlerteCovid_Affaires/InfoRegion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlerteCovid_Affaires
+{
+    public class InfoRegion : INotifyPropertyChanged
+    {
+        private string _nomRegion;
+        private int _nbEclosions;
+
+        public string NomRegion { get => _nomRegion; set => _nomRegion = value; }
+
+        public int NbEclosions
+        {
+            get => _nbEclosions;
+            set { _nbEclosions = value;
+                this.notifier("NbEclosions");
+                this.notifier("CouleurEtatAlerte");
+                this.notifier("NomEtatAlerte");
+            }
+        }
+        public string CouleurEtatAlerte
+        {
+            get
+            {
+                if (_nbEclosions <= 25)
+                {
+                    return "LightGreen";
+                }
+                else if (_nbEclosions <= 50)
+                {
+                    return "Yellow";
+                }
+                else if (_nbEclosions <= 100)
+                {
+                    return "Orange";
+                }
+                else return "Red";
+            }
+        }
+        public string NomEtatAlerte
+        {
+            get
+            {
+                if (_nbEclosions <= 25)
+                {
+                    return ("Vigilante");
+                }
+                else
+                if (_nbEclosions <= 50)
+                {
+                    return ("Préalerte");
+                }
+                else
+                if (_nbEclosions <= 100)
+                {
+                    return ("Alerte modérée");
+                }
+                else
+                    return ("Alerte maximale");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void notifier(string propriete) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propriete));
+        }      
+        
+        public InfoRegion(string nom, int nbNouveauxCas)
+        {
+            _nomRegion = nom;
+            _nbEclosions = nbNouveauxCas;
+        }
+    }
+}
---
+++ labo1a-alertecovid-Samuel-Bourque/AlerteCovid_UI/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using AlerteCovid_Affaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlerteCovid_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<InfoRegion> Regions { get; }
+        private Random _random;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _random = new Random();
+            Regions = new List<InfoRegion>() {
+              new InfoRegion("01- Bas Saint-Laurent", _random.Next(1,150)),
+              new InfoRegion("03- Capitale Nationale", _random.Next(1,150)),
+              new InfoRegion("08- Montréal", _random.Next(1,150)),
+              new InfoRegion("12- Chaudière-Appalaches", _random.Next(1,150))
+            };
+
+            grilleAlerteCovid.DataContext = this;
+
+            Timer chrono = new Timer();
+            chrono.Interval = 2000;
+            chrono.Enabled = true;
+            chrono.Elapsed += new ElapsedEventHandler(genererNouvelleValeur);
+        }
+        private void genererNouvelleValeur(object sender, ElapsedEventArgs e)
+        {
+            foreach (InfoRegion uneRegion in Regions)
+                uneRegion.NbEclosions = _random.Next(1, 150);
+        }
+    }
+}
